ffd79a101e9fbfea0e5ccd631c2f6fc03c3091d6
Andrew Liu <Andrew@Andrews-MacBook-Pro.local>
2014-12-16 19:14:10 -0800

    Initial Commit

---
+++ HelloWatchKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWatchKit
+//
+//  Created by Andrew Liu on 12/16/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWatchKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWatchKit
+//
+//  Created by Andrew Liu on 12/16/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
